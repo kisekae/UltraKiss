68a3e23a0a43275d42a4356812b1dbb90ba88aca
William <William@SIRIUS>
2026-05-17 14:34:00 -0600

    UltraKiss 5.3 update 2b

--- KisekaeUltraKiss/src/main/java/com/wmiles/kisekaeultrakiss/Kisekae/Kisekae.java
+++ KisekaeUltraKiss/src/main/java/com/wmiles/kisekaeultrakiss/Kisekae/Kisekae.java
@@ -203,7 +203,7 @@ public class Kisekae extends Applet
 
       LogFile.start() ;
       builddate = Calendar.getInstance() ;
-      builddate.set(2026,5-1,16) ;
+      builddate.set(2026,5-1,17) ;
       
       // Restore the properties.
       
